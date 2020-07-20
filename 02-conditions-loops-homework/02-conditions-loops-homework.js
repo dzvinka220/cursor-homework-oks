a94c2b6b0fcd2d71a932a158feff71e300344d28
dzvinka220 <68071721+dzvinka220@users.noreply.github.com>
2020-07-20 16:18:51 +0300

    Add files via upload

--- 02-conditions-loops-homework/02-conditions-loops-homework.js
+++ 02-conditions-loops-homework/02-conditions-loops-homework.js
@@ -13,18 +13,22 @@ do {
      alert("Потрібно ввести ціле число");
    }
   } while (!Number.isInteger(numbM) || numbM === 0);
-  
-console.log(Number.isInteger(numbN));
-console.log(Number.isInteger(numbM));
+  console.log("Перше число " + numbN);
+  console.log("Друге число " + numbM);
+//console.log(Number.isInteger(numbN));
+//console.log(Number.isInteger(numbM));
 
 oddNumb = confirm ("Пропускати парні?");
-console.log(oddNumb);
+console.log("Пропуск парних чисел " + oddNumb);
 
 let sumI = 0;
 let i = 0;
 for (i = numbN; i <= numbM; i++){
   if (oddNumb !== false && i % 2 == 0) continue;
   else{ sumI = sumI + i; 
-  console.log(sumI);}
+    //console.log(sumI);
+  }
 };
 
+console.log(`Сума чисел від  ${numbN} до ${numbM} = ${sumI}`);
+alert(`Сума чисел від  ${numbN} до ${numbM} = ${sumI}`);
