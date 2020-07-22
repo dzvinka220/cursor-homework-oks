e447b69251485a79afade65eebee62180cdef5ff
dzvinka220 <68071721+dzvinka220@users.noreply.github.com>
2020-07-23 02:30:23 +0300

    Add files via upload

---
+++ 03-functions-homework/03-functions-homework.js
@@ -0,0 +1,32 @@
+/*function getMaxDigit(number){
+ 
+    number = number.toString();
+    let numberArr = Array.from(number);
+     console.log(numberArr.sort());
+        return numberArr.pop();
+        
+    
+    }
+    console.log(getMaxDigit(0123456789));
+*/
+
+/*function getPowerNumber (number, power){
+    let i=0; 
+    let total;
+   let numbArr=[];
+     
+       while(i<power){
+         i++;
+         numbArr.push(number);
+         //console.log(result);
+         
+        total = numbArr.reduce( ( total, amount ) => total * amount );
+      // console.log(total);
+     
+   
+   }
+     return total;
+   }
+       
+   console.log(getPowerNumber(5, 3))*/
+   
