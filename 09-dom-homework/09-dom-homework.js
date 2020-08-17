bb1c9c33d759cc19f848151066c687725df3807f
dzvinka220 <68071721+dzvinka220@users.noreply.github.com>
2020-08-17 23:50:35 +0300

    Add files via upload

---
+++ 09-dom-homework/09-dom-homework.js
@@ -0,0 +1,26 @@
+function generateColor() {
+    return '#' + Math.floor(Math.random()*16777215).toString(16) // 16777215.toString(16)==ffffff
+  };
+
+function createTable(){
+    const table = document.createElement("table");
+    document.body.append(table);
+    for (let i=0; i<5; i++){
+      let tr = document.createElement("tr");
+      table.append(tr);
+      for (let i=0; i<5; i++){
+        let td = document.createElement("td");
+        tr.append(td);
+      }
+    }
+  }
+function setColor(){
+  let box = document.querySelectorAll("td");
+  box.forEach(element => {
+    element.setAttribute("style", `background: ${generateColor()}; border: 9px dotted ${generateColor()}; height = 50 px;`);
+  });
+  return box;
+}  
+setInterval (setColor, 1000);
+createTable();
+setColor();
